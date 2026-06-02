78ab04d7f4145ad5d3b8557a0acb8730e03a0d34
Quedin Vanegas <qavanegasr@udistrital.edu.co>
2026-06-02 00:04:31 -0500

    [MVC] Bloque 2 - Creacion del modelo Personaje

---
+++ RPG_Manager/src/rpg/model/Personaje.java
@@ -0,0 +1,26 @@
+package rpg.model;
+
+public class Personaje {
+    private int id;
+    private String nombre;
+    private String tipo;
+    private int nivel;
+    public Personaje() {}
+    public Personaje(int id, String nombre, String tipo, int nivel) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public int getNivel() { return nivel; }
+    public void setNivel(int nivel) { this.nivel = nivel; }
+}
